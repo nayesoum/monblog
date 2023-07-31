0fb9fa6b8383ac4c376bb19311c20e58b9cc6f22
nayesoum <104022857+nayesoum@users.noreply.github.com>
2023-07-31 17:04:33 +0200

    modification de tous les formulaire mise en page et ajout des svg

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,8 +17,26 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('Content')
+            ->add('title', TextType::class,[
+                'attr' =>[
+                    'class'=>'form-control',
+                    'minlenght' =>'2',
+                    'maxlenght' =>'200',
+                ],
+                'label'=>'Titre de l article',
+                'label_attr' =>[
+                    'class'=> 'form-label mt-3'
+                ]
+            ])
+            ->add('Content', TextareaType::class,[
+                'attr' =>[
+                    'class'=>'form-control',
+                ],
+                'label'=>'Description',
+                'label_attr' =>[
+                    'class'=> 'form-label mt-3'
+                ]
+            ])
              ->add('image', 
              //FileType::class,
             // [
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,16 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class,[
+                'attr' =>[
+                    'class' => 'form-control',
+                ],
+                'label' => 'Titre de la catégorie',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4',
+                ]
+            ])
+            
         ;
     }
 
